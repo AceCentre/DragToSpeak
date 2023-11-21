b2f9edf998efdbbe88fdebe99ac246c4027744e5
will wade <willwade@gmail.com>
2023-11-21 09:43:27 +0000

    Initial Commit

---
+++ DragToSpeak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragToSpeak
+//
+//  Created by Will Wade on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragToSpeak/DragToSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragToSpeakApp.swift
+//  DragToSpeak
+//
+//  Created by Will Wade on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DragToSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
