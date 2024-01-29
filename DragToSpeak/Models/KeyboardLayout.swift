026d0412367bd8cd533d4776b3cafaf318497f76
will wade <willwade@gmail.com>
2024-01-29 11:07:00 +0000

    removal of dud files

--- DragToSpeak/Models/KeyboardLayout.swift
+++
@@ -1,8 +0,0 @@
-//
-//  KeyboardLayout.swift
-//  DragToSpeak
-//
-//  Created by Will Wade on 26/01/2024.
-//
-
-import Foundation
--- DragToSpeak/Models/UserSettings.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserSettings.swift
-//  DragToSpeak
-//
-//  Created by Will Wade on 26/01/2024.
-//
-
-import Foundation
