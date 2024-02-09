cc5ee26555f05e0c757da8e1ff575031614d08da
will wade <willwade@gmail.com>
2024-02-09 22:29:31 +0000

    bad attempt at making dark mode work
    im not sure these colours are right. hoping this doesn't break anything

--- DragToSpeak/Components/CellView.swift
+++ DragToSpeak/Components/CellView.swift
@@ -16,14 +16,48 @@ struct CellView: View {
     var cellHeight: CGFloat
 
     @EnvironmentObject var appSettings: AppSettings
+    @Environment(\.colorScheme) var colorScheme // Add this line to detect color scheme
     
-    var body: some View {        
+    var body: some View {
         let fontSize = isPending ? appSettings.enlargeKeys ? 50 : appSettings.fontSize : appSettings.fontSize
         
         Text(text)
             .frame(width: cellWidth, height: cellHeight)
             .border(.black)
-            .background(isActive ? .green : isPending ? .yellow.opacity(0.5) : cellType == .finish ? .green.opacity(0.5) : cellType == .space ? .blue.opacity(0.5) : .white) // This line is crazy, dont ask
+            .background(backgroundColor) // Adjusted to use colorScheme
+            .foregroundColor(foregroundColor) // Adjusted to use colorScheme
             .font(.system(size: CGFloat(fontSize)))
+    }
+
+    // Adjusted to consider color scheme for background color
+    private var backgroundColor: Color {
+        let baseColor: Color
+        if isActive {
+            baseColor = .green
+        } else if isPending {
+            baseColor = .yellow.opacity(0.5)
+        } else {
+            switch cellType {
+            case .finish:
+                baseColor = .green.opacity(0.5)
+            case .space:
+                baseColor = .blue.opacity(0.5)
+            default:
+                baseColor = .white
+            }
+        }
+        
+        // Adjust baseColor for dark mode if necessary
+        return colorScheme == .dark ? baseColor.opacity(0.5) : baseColor // Example adjustment
+    }
+
+    // Adjusted to consider color scheme for foreground color
+    private var foregroundColor: Color {
+        switch backgroundColor {
+        case .green, .blue, .yellow:
+            return colorScheme == .dark ? .white : .black // Adjust based on color scheme
+        default:
+            return colorScheme == .dark ? .white : .primary // Adjust for dark mode
+        }
     }
 }
