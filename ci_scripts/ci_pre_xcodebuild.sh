f7eeb82422678b385540bfcb9eec173c7a7518d3
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-02-09 14:52:12 +0000

    Trail and error

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -10,7 +10,9 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+#cd $CI_WORKSPACE/ci_scripts || exit 1
+
+pwd
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"AZURE_USER_PASS\":\"%s\"}" "$AZURE_USER_PASS" >> ../secrets.json
